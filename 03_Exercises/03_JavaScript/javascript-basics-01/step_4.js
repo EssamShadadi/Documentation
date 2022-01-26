065d1a17987f0a1a06c411a8973506eb75474ce5
EssamShadadi <essam.shadadi@gmail.com>
2022-01-26 15:53:34 +0200

    js-basics step 4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+function promptName(){
+    let name=document.getElementById("name").value
+    let surname= document.getElementById("surname").value
+    let city = document.getElementById("city").value
+    alert("name: " +name+"\n"+"Prenom: "+surname+"\n"+"Ville: "+city)
+}
